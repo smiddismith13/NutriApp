bfa166e101e7a2d78d36a0824315ef4ad46cbcbf
smiddismith13 <smiddismith13@gmail.com>
2025-10-06 20:13:55 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hallo 👋 Willkommen bei NutriApp!</Text>
+      <Text>Deine ErnährungsApp für Alltag und Gesundheit.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
